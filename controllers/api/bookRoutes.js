482e4194a1bf3b43a3d3c1e69ae0a55cb23073cc
Mark Tovar <markjtovar24@gmail.com>
2024-03-25 21:40:45 -0500

    added create and delete functionality into controllers/api/bookRoutes

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
 const { Book } = require('../../models/Book');
 
-// //CREATE (add) books to shelf
-// router.post('/', async (req, res) => { 
-//     try {
-//         const bookData = await Book.create(req.body);
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// })
+//CREATE (add) books to shelf
+router.post('/', async (req, res) => { 
+    try {
+        const bookData = await Book.create(req.body);
+        res.status(200).json(bookData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//DELETE books from shelf
+router.delete('/:id', async (req, res) => {
+    try {
+        const bookData = await Book.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if(!bookData) {
+            res.status(404).json({ message: "No books with this ID found!"});
+            return;
+        }
+        res.status(200).json(bookData);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+//UPDATE
+
+module.exports = router;
