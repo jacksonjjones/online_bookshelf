53df1a3314ab33c7c2e7478d27ab42ac3d39ed0f
annajantzen <aejantz2015@gmail.com>
2024-03-27 19:58:48 -0500

    updated seeds js and added user data

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,25 +1,22 @@
 const sequelize = require('../config/connection');
-const { Book } = require('../models/Book');
-
-const  bookData = require('./bookData.json');
-// const  = require('./.json');
+const { Book } = require('../models');
+const bookData = require('./bookData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  try {
+    await sequelize.sync({ force: true });
 
-  const books = await Book.bulkCreate(Data, {
-    individualHooks: true,
-    returning: true,
-  });
+    const books = await Book.bulkCreate(bookData, {
+      individualHooks: true,
+      returning: true,
+    });
 
-  // for (const post of Data) {
-  //   await create({
-  //     ...
-  //   //   user_id: users[Math.floor(Math.random() * users.length)].id,
-  //   });
-  // }
+    // Additional logic can be added here if needed
 
-  process.exit(0);
+    console.log('Database seeded successfully');
+  } catch (error) {
+    console.error('Error seeding database:', error);
+  }
 };
 
 seedDatabase();
